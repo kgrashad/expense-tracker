bdafe4d52a60e0d0f80fb383b1fd1ef5f7d64df8
Khaled Rashad <krashad@microsoft.com>
2015-12-10 18:35:12 +0200

    4. forms and events

--- expense-tracker.js
+++ expense-tracker.js
@@ -4,7 +4,39 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     expenses: function(){
-      return Expenses.find({});
+      return Expenses.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-expense": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var target = event.target,
+          text = target.text.value,
+          category = target.category.value,
+          value = target.value.value;
+
+      // Insert an expense into the collection
+      Expenses.insert({
+        text: text,
+        category: category,
+        value: value,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+      event.target.category.value = "";
+      event.target.value.value = "";
+    }
+  });
+
+  Template.expense.events({
+    "click .delete": function () {
+      Expenses.remove(this._id);
     }
   });
 }
