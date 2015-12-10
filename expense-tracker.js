9aee3b700b14321bb54ebaa985e11e647989e88d
Khaled Rashad <krashad@microsoft.com>
2015-12-10 18:34:18 +0200

    2. expense template

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,23 +1,10 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  // This code only runs on the client
+  Template.body.helpers({
+    expenses: [
+      { text: "My first expense", category: "food", value: 10 },
+      { text: "My first expense", category: "sports", value: 100 },
+      { text: "My third expense", category: "education", value: 50 }
+    ]
   });
 }
