ac237a798fecb20d8b3c0c6ee01fe6892e2f4354
Khaled Rashad <krashad@microsoft.com>
2015-12-10 18:34:44 +0200

    3. collections

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,10 +1,10 @@
+Expenses = new Mongo.Collection("expenses");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    expenses: [
-      { text: "My first expense", category: "food", value: 10 },
-      { text: "My first expense", category: "sports", value: 100 },
-      { text: "My third expense", category: "education", value: 50 }
-    ]
+    expenses: function(){
+      return Expenses.find({});
+    }
   });
 }
